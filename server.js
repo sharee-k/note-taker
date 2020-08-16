9e52d5692d988912c31f8cdb40b6ec09b5543351
Sharee Burkeen <shareeburkeen@yahoo.com>
2020-08-16 13:40:09 -0500

    set up server and add heroku

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+//const apiRoutes = require('./routes/apiRoutes');
+//const htmlRoutes = require('./routes/htmlRoutes');
 
 //parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
 app.use(express.json());
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes)
+//app.use('/api', apiRoutes);
+//app.use('/', htmlRoutes)
 
 app.use(express.static('public'));
 
